1d6e90063df9bf707a9cb4f327397e891fab7865
admkrl1 <162460553+admkrl1@users.noreply.github.com>
2024-03-19 21:36:12 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    int a=5;
+    int b;
+    b=a+1;
+    System.out.println(b);
+    System.out.println(b*a);
+    
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
